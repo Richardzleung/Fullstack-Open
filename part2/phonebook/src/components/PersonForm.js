959b59230e73d2c25f8ef055ffdcc2f0a999ff12
Richardzleung <61563481+Richardzleung@users.noreply.github.com>
2020-09-23 15:18:59 -0700

    Update PersonForm.js
    refactored to include method addName in component

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,21 +1,35 @@
 import React from 'react'
 
 const PersonForm = (props) => {
+  const {persons,setNewFilter,setNewName,
+          setNewNumber, setPersons, newName, newNumber, 
+          handleNameChange,handleNumberChange} = props
+
+  const addName= (event) => {   
+    event.preventDefault()
+    if (persons.some(e => e.name === newName)) {
+      window.alert(`${newName} is already added to phonebook`)
+    } 
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }    
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+      setNewFilter('')
+    }
+  }
     return (
-    <form onSubmit={props.addName}>
+    <form onSubmit={addName}>
         <div>
           name: 
-          <input 
-            value={props.newName}
-            onChange={props.handleNameChange}
-          />
+          <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           number:
-          <input
-            value={props.newNumber}
-            onChange={props.handleNumberChange}
-          />
+          <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">
