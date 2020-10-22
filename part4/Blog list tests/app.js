6ef402e5f03646792a32e6ed651a2595ee234090
Richardzleung <61563481+Richardzleung@users.noreply.github.com>
2020-10-22 15:41:03 -0700

    Update app.js

--- part4/Blog list tests/app.js
+++ part4/Blog list tests/app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 require('express-async-errors')
 const blogRouter = require('./controllers/blog')
-const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -23,7 +22,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
 
 app.use(middleware.unknownEndpoint)
